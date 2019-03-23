89141732ba23a8e11448f2503a80bce07e14d6f9
顾咏 <zhijiang.nzj@alibaba-inc.com>
2019-03-23 17:03:28 +0800

    Update Apis

--- V20150227/Api.php
+++ V20150227/Api.php
@@ -3,6 +3,7 @@
 namespace AlibabaCloud\Yundun\V20150227;
 
 use AlibabaCloud\ApiResolverTrait;
+use AlibabaCloud\Rpc;
 
 /**
  * Resolve Api based on the method name.
@@ -21,4 +22,59 @@ use AlibabaCloud\ApiResolverTrait;
 class YundunApiResolver
 {
     use ApiResolverTrait;
+}
+
+class V20150227Rpc extends Rpc
+{
+    /** @var string */
+    public $product = 'Yundun';
+
+    /** @var string */
+    public $version = '2015-02-27';
+
+    /** @var string */
+    public $method = 'POST';
+
+    /** @var string */
+    public $serviceCode = 'yundun';
+}
+
+class TodayAllkbps extends V20150227Rpc
+{
+}
+
+class TodayAegisOnlineRate extends V20150227Rpc
+{
+}
+
+class CurrentDdosAttackNum extends V20150227Rpc
+{
+}
+
+class AllMalwareNum extends V20150227Rpc
+{
+}
+
+class TodayMalwareNum extends V20150227Rpc
+{
+}
+
+class TodayCrackIntercept extends V20150227Rpc
+{
+}
+
+class TodayBackdoor extends V20150227Rpc
+{
+}
+
+class TodayAllpps extends V20150227Rpc
+{
+}
+
+class WebAttackNum extends V20150227Rpc
+{
+}
+
+class TodayqpsByRegion extends V20150227Rpc
+{
 }
